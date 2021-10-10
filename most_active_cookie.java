bd19808fd4e0a340fd074a87504e4c00bf50e24f
nvanny <nvanny@ucsd.edu>
2021-10-09 23:51:22 -0700

    migrated some into different functions

--- most_active_cookie.java
+++ most_active_cookie.java
@@ -4,7 +4,7 @@ import java.io.*;
 class most_active_cookie {
 
     static HashMap<String, Integer> cookieMap = new HashMap<String, Integer>();
-    public static void main(String[] args) throws FileNotFoundException{
+    public static void main(String[] args) throws FileNotFoundException {
         // checking for all necessary params
         // System.out.println(args.length);
         if(args.length != 3) {
@@ -16,15 +16,27 @@ class most_active_cookie {
             System.err.println("Check usage, -d flag needed");
         } 
 
+        // parses through the csv via a scanner and creates a list to represent the csv
         String date = args[2];
-        List<String> csv = new ArrayList<>();
-        Scanner scan = new Scanner(new FileReader(args[0]));
+
+        List<String> csv = convertCSV(date, args[0]);
+
+        printCookies(date, csv);
+    }
+
+    public static List<String> convertCSV(String date, String filename) throws FileNotFoundException {
+        List<String> csv = new ArrayList<String>();
+        Scanner scan = new Scanner(new FileReader(filename));
         scan.useDelimiter("\n");
         while(scan.hasNext()) {
             csv.add(scan.next());
         }
         scan.close();
+        return csv;
+    }
 
+    public static void printCookies(String date, List<String> csv) {
+        // gets the most active cookies and prints them
         List<String> mostActiveCookies = getMostActive(date, csv);
         for(String cookie : mostActiveCookies) {
             System.out.println(cookie);
@@ -34,6 +46,8 @@ class most_active_cookie {
     public static List<String> getMostActive(String date, List<String> csv) {
         List<String> cookieList = new ArrayList<>();
         int max = 0;
+
+        // creating HashMap that contains values for valid cookies matching the timestamp
         for(String cookie : csv) {
             String[] cookieSplit = cookie.split(",");
             String cookieValue = cookieSplit[0];
@@ -44,6 +58,8 @@ class most_active_cookie {
                 max = Math.max(cookieMap.get(cookieValue), max);
             }
         }
+
+        // creates list of valid cookies
         for(String cookieKey : cookieMap.keySet()) {
             if(cookieMap.get(cookieKey) >= max) {
                 cookieList.add(cookieKey);
